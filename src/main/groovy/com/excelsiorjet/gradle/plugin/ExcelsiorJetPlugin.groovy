5ba1c50092269c0d40700dee5653cab4f06d5861
Aleksey Zhidkov <aleksey.zhidkov@gmail.com>
2016-06-22 17:05:19 +0600

    Remove unused code

--- src/main/groovy/com/excelsiorjet/gradle/plugin/ExcelsiorJetPlugin.groovy
+++ src/main/groovy/com/excelsiorjet/gradle/plugin/ExcelsiorJetPlugin.groovy
@@ -1,16 +1,9 @@
 package com.excelsiorjet.gradle.plugin
 
-import com.excelsiorjet.api.log.Log
-import com.excelsiorjet.api.tasks.ApplicationType
-import com.excelsiorjet.api.tasks.ClasspathEntry
 import com.excelsiorjet.api.tasks.JetProject
-import com.excelsiorjet.api.util.Txt
-import org.gradle.api.GradleScriptException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.logging.LogLevel
-
-import java.util.stream.Stream
 
 /**
  * Excelsior JET plugin. Allows to compile project into native executable and pack it to native for platform package.
