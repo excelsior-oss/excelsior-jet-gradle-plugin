01d8aed675ec220630ffa1306576eb43479cd18f
Aleksey Zhidkov <aleksey.zhidkov@gmail.com>
2016-06-22 16:38:03 +0600

    Polish workaround for bug in supporot of maven relocate in gradle

--- src/main/groovy/com/excelsiorjet/gradle/plugin/ExcelsiorJetPlugin.groovy
+++ src/main/groovy/com/excelsiorjet/gradle/plugin/ExcelsiorJetPlugin.groovy
@@ -4,6 +4,8 @@ import com.excelsiorjet.api.log.Log
 import com.excelsiorjet.api.tasks.ApplicationType
 import com.excelsiorjet.api.tasks.ClasspathEntry
 import com.excelsiorjet.api.tasks.JetProject
+import com.excelsiorjet.api.util.Txt
+import org.gradle.api.GradleScriptException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.logging.LogLevel
@@ -23,7 +25,8 @@ class ExcelsiorJetPlugin implements Plugin<Project> {
     @Override
     void apply(Project target) {
         target.getLogging().captureStandardError(LogLevel.INFO)
-        Log.logger = new GradleLog(target.getLogger())
+        def log= new GradleLog(target.getLogger())
+        JetProject.configureEnvironment(log, ResourceBundle.getBundle("GradleStrings", Locale.ENGLISH))
 
         target.getExtensions().create(ExcelsiorJetExtension.EXTENSION_NAME, ExcelsiorJetExtension)
 
@@ -58,10 +61,18 @@ class ExcelsiorJetPlugin implements Plugin<Project> {
     static List<ClasspathEntry> getDependencies(Project project) {
         def configuration = project.configurations.getByName("compile")
         return configuration.getDependencies().collect {
+            // due to bug in support of maven relocation in gradle, dependency may be resolved into multiple files
+            // https://issues.gradle.org/browse/GRADLE-2812
+            // https://issues.gradle.org/browse/GRADLE-3301
+            // https://discuss.gradle.org/t/oddity-in-the-output-of-dependencyinsight-task/7553/12
+            // https://discuss.gradle.org/t/warning-after-gradle-update-1-9-with-pmd-plugin/1731/3
             def depFiles = configuration.files(it)
-            if (depFiles.size() > 0) {
-                new ClasspathEntry(depFiles.first(), project.group.equals(it.group))
+            if (depFiles.size() == 0) {
+                throw new GradleScriptException(Txt.s("ExcelsiorJetGradlePlugin.NoFilesForDependency", it), null)
+            } else if (depFiles.size() > 1) {
+                Log.logger.warn(Txt.s("ExcelsiorJetGradlePlugin.MultipleFilesForDependency", it, depFiles, depFiles.first()))
             }
+            new ClasspathEntry(depFiles.first(), project.group.equals(it.group))
         }
     }
 }
