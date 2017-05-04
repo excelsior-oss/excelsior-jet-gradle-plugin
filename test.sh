cc2bb7706636adffb7a1a42f77a6ce717831f76b
kit <nlipsky@excelsior-usa.com>
2017-05-04 17:56:46 +0700

    Bash script added to test the plugin against different Excelsior JET versions and Tomcats

---
+++ test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+export JAVA_HOME=/usr/java/jdk1.8.0_25
+home=/home/user
+tests[0]="${home}/jet11.0-std-x86;notomcat"
+tests[1]="${home}/jet11.0-pro-x86;notomcat"
+tests[2]="${home}/jet11.0-ent-x86;${home}/apache-tomcat-6.0.18"
+tests[3]="${home}/jet11.0-pro-amd64;notomcat"
+tests[4]="${home}/jet11.0-ent-amd64;${home}/apache-tomcat-7.0.62"
+tests[5]="${home}/jet11.3-ent-amd64;${home}/apache-tomcat-8.0.26"
+tests[6]="${home}/jet11.3-pro-amd64;notomcat"
+tests[7]="${home}/jet11.3-ent-x86;${home}/apache-tomcat-6.0.18"
+tests[8]="${home}/jet11.3-pro-x86;notomcat"
+tests[9]="${home}/jet11.3-std-x86;notomcat"
+tests[10]="${home}/jet11.3-eval-amd64;${home}/apache-tomcat-8.0.26"
+
+for t in ${tests[*]};
+do
+  test=(${t//;/ })
+  echo Testing Excelsior JET - ${test[0]} with Tomcat - ${test[1]}
+  ./gradlew clean build  -Djet.home=${test[0]} -Dtomcat.home=${test[1]}
+done 
